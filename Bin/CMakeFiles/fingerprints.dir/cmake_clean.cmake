3516343296f4a31c622d7a6cc524c781135e1570
ThibaultCastells <38716394+ThibaultCastells@users.noreply.github.com>
2019-03-02 20:38:17 +0100

    add Bin and Build directories

---
+++ Bin/CMakeFiles/fingerprints.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fingerprints.dir/src/starter1.cpp.o"
+  "CMakeFiles/fingerprints.dir/src/coord.cpp.o"
+  "fingerprints.pdb"
+  "fingerprints"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fingerprints.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
